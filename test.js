bf8389910ce3d35b9f30f98f0b1ab903f7f89243
Samim Mirhosseini <ssmirr@users.noreply.github.com>
2018-11-06 00:08:22 -0500

    refactor: add better tests using mocha

--- test.js
+++ test.js
@@ -1,30 +1,77 @@
 const semverCompare = require('./semver-range').semverCompare;
 const semverDiff = require('./semver-range').semverDiff;
 
-console.log('^2.0.0 AND ~2.0.1: ', semverCompare('^2.0.0', '~2.0.1'), '~', semverDiff('^2.0.0', '~2.0.1'));
-console.log('=========================================================')
-console.log( '>=2.0.0 AND >=2.0.1: ', semverCompare('>=2.0.0', '>=2.0.1'), '~', semverDiff('>=2.0.0', '>=2.0.1'));
-console.log('=========================================================')
-console.log('1.2.3-alpha.3 AND 1.2.3: ', semverCompare('1.2.3-alpha.3', '1.2.3'), '~', semverDiff('1.2.3-alpha.3', '1.2.3'));
-console.log('=========================================================')
-console.log('1.2.3 AND 1.2.3-alpha.3: ', semverCompare('1.2.3', '1.2.3-alpha.3'), '~', semverDiff('1.2.3', '1.2.3-alpha.3'));
-console.log('=========================================================')
-console.log('1.2.3-alpha.3 AND 2.2.3: ', semverCompare('1.2.3-alpha.3', '2.2.3'), '~', semverDiff('1.2.3-alpha.3', '2.2.3'));
-console.log('=========================================================')
-console.log('2.2.3 AND 1.2.3-alpha.3: ', semverCompare('2.2.3', '1.2.3-alpha.3'), '~', semverDiff('2.2.3', '1.2.3-alpha.3'));
-console.log('=========================================================')
-console.log('<=1.1.0 >0.2.0 AND <=0.1.1 >1.2.0: ', semverCompare('<=1.1.0 >0.2.0', '<=0.1.1 >1.2.0'), '~', semverDiff('<=1.1.0 >0.2.0', '<=0.1.1 >1.2.0'));
-console.log('=========================================================')
-console.log('^4.0.x AND 4.x: ', semverCompare('^4.0.x', '4.x'), '~', semverDiff('^4.0.x', '4.x'));
-console.log('=========================================================')
-console.log('4.0.x AND 4.x: ', semverCompare('4.0.x', '4.x'), '~', semverDiff('4.0.x', '4.x'));
-console.log('=========================================================')
-console.log('^4.x AND 4.0.x: ', semverCompare('4.x', '4.0.x'), '~', semverDiff('4.x', '4.0.x'));
-console.log('=========================================================')
-console.log('4.x AND <URL>: ', semverCompare('4.x', 'https://github.com/indexzero/forever/tarball/v0.5.6'), '~', semverDiff('4.x', 'https://github.com/indexzero/forever/tarball/v0.5.6'));
-console.log('=========================================================')
-console.log('<URL> AND 4.x: ', semverCompare('https://github.com/indexzero/forever/tarball/v0.5.6', '4.x'), '~', semverDiff('https://github.com/indexzero/forever/tarball/v0.5.6', '4.x'));
-console.log('=========================================================')
-console.log('4.0.0 AND 3.0.0: ', semverCompare('4.0.0', '3.0.0'), '~', semverDiff('4.0.0', '3.0.0'));
-console.log('=========================================================')
-console.log('3.0.0 AND 4.0.0: ', semverCompare('3.0.0', '4.0.0'), '~', semverDiff('3.0.0', '4.0.0'));
+const assert = require('assert');
+
+describe('Semver range comparisons', function () {
+    
+    it('^2.0.0 -> ~2.0.1 is a major downgrade', function () {
+        assert.equal(semverCompare('^2.0.0', '~2.0.1'), 'downgraded');
+        assert.equal(semverDiff('^2.0.0', '~2.0.1'), 'major');
+    });
+
+    it('>=2.0.0 -> >=2.0.1 is an upgrade', function () {
+        assert.equal(semverCompare('>=2.0.0', '>=2.0.1'), 'upgraded');
+        assert.equal(semverDiff('>=2.0.0', '>=2.0.1'), 'other');
+    });
+
+    it('1.2.3-alpha.3 -> 1.2.3 is a prerelease upgrade', function () {
+        assert.equal(semverCompare('1.2.3-alpha.3', '1.2.3'), 'upgraded');
+        assert.equal(semverDiff('1.2.3-alpha.3', '1.2.3'), 'prerelease');
+    });
+
+    it('1.2.3 -> 1.2.3-alpha.3 is a prerelease downgrade', function () {
+        assert.equal(semverCompare('1.2.3', '1.2.3-alpha.3'), 'downgraded');
+        assert.equal(semverDiff('1.2.3', '1.2.3-alpha.3'), 'prerelease');
+    });
+
+    it('1.2.3-alpha.3 -> 2.2.3 is a major upgrade', function () {
+        assert.equal(semverCompare('1.2.3-alpha.3', '2.2.3'), 'upgraded');
+        assert.equal(semverDiff('1.2.3-alpha.3', '2.2.3'), 'premajor');
+    });
+
+    it('2.2.3 -> 1.2.3-alpha.3 is a premajor downgrade', function () {
+        assert.equal(semverCompare('2.2.3', '1.2.3-alpha.3'), 'downgraded');
+        assert.equal(semverDiff('2.2.3', '1.2.3-alpha.3'), 'premajor');
+    });
+
+    it('<=1.1.0 >0.2.0  ->  <=0.1.1 >1.2.0 is a major upgrade', function () {
+        assert.equal(semverCompare('<=1.1.0 >0.2.0', '<=0.1.1 >1.2.0'), 'upgraded');
+        assert.equal(semverDiff('<=1.1.0 >0.2.0', '<=0.1.1 >1.2.0'), 'major');
+    });
+
+    it('^4.0.x -> 4.x is not_changed', function () {
+        assert.equal(semverCompare('^4.0.x', '4.x'), 'not_changed');
+        assert.equal(semverDiff('^4.0.x', '4.x'), 'other');
+    });
+
+    it('4.0.x -> 4.x is a major upgrade', function () {
+        assert.equal(semverCompare('4.0.x', '4.x'), 'upgraded');
+        assert.equal(semverDiff('4.0.x', '4.x'), 'major');
+    });
+
+    it('^4.x -> 4.0.x is a major downgrade', function () {
+        assert.equal(semverCompare('4.x', '4.0.x'), 'downgraded');
+        assert.equal(semverDiff('4.x', '4.0.x'), 'major');
+    });
+
+    it('4.x -> <URL> is url', function () {
+        assert.equal(semverCompare('4.x', 'https://github.com/indexzero/forever/tarball/v0.5.6'), 'url');
+        assert.equal(semverDiff('4.x', 'https://github.com/indexzero/forever/tarball/v0.5.6'), 'url');
+    });
+
+    it('<URL> -> 4.x is url', function () {
+        assert.equal(semverCompare('https://github.com/indexzero/forever/tarball/v0.5.6', '4.x'), 'url');
+        assert.equal(semverDiff('https://github.com/indexzero/forever/tarball/v0.5.6', '4.x'), 'url');
+    });
+
+    it('4.0.0 -> 3.0.0 is a major downgrade', function () {
+        assert.equal(semverCompare('4.0.0', '3.0.0'), 'downgraded');
+        assert.equal(semverDiff('4.0.0', '3.0.0'), 'major');
+    });
+
+    it('3.0.0 AND 4.0.0 is a major upgrade', function () {
+        assert.equal(semverCompare('3.0.0', '4.0.0'), 'upgraded');
+        assert.equal(semverDiff('3.0.0', '4.0.0'), 'major');
+    });
+});
